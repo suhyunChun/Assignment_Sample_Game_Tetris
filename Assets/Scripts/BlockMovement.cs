190be1c6e9bda05c98d1a1a4f76c8bdb7eea654d
suhyunChun <suhyun.chun@stonybrook.edu>
2023-12-27 19:14:27 +0900

    [Add] add prefabs tetromino & block movement , fall

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class BlockMovement : MonoBehaviour
 {
 
-    private float fallingT = 0.5f;
+    private float fallingSpeed = 0.5f;
+    private float fall = 0; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,36 +16,54 @@ public class BlockMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeBlockLocation();
     }
-
+    private bool isValid()
+    {
+        return true;
+    }
     private void ChangeBlockLocation()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
 
-        }else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
+        // input값이 valid한지 먼저 체크 한 후 진행 
 
-        }else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
+        if (!isValid())
+            return;
 
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time- fall >= fallingSpeed)
+        {
+            MoveDown();
+            fall = Time.time;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Rotate();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
         }
     }
     private void MoveDown()
     {
-
+        transform.position += new Vector3(0, -1, 0);
     }
-    private void MoveUp()
+    private void Rotate()
     {
+        transform.Rotate(0, 0, 90);
 
     }
     private void MoveLeft()
     {
-
+        transform.position += new Vector3(-1, 0, 0);
     }
     private void MoveRight()
     {
-
+        transform.position += new Vector3(1, 0, 0);
     }
 }
