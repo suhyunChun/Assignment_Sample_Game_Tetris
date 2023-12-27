6bb9f85607760bbb1143e883ac07eabd7c75cd3b
suhyunChun <suhyun.chun@stonybrook.edu>
2023-12-27 17:42:36 +0900

    [Add] add prefabs tetromino

---
+++ Assets/Scripts/BlockMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMovement : MonoBehaviour
+{
+
+    private float fallingT = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ChangeBlockLocation()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+    }
+    private void MoveDown()
+    {
+
+    }
+    private void MoveUp()
+    {
+
+    }
+    private void MoveLeft()
+    {
+
+    }
+    private void MoveRight()
+    {
+
+    }
+}
